cc09ef118f131e9e248f475f93aa688110e5b6ee
MichelLyJow <michellyjow@gmail.com>
2026-09-15 12:22:16 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -57,7 +57,7 @@ Tabs.Main:AddButton({
     Title = "Michel x Ride A Pet",
     Callback = function()
         CloseAllGUI()
-        loadstring(game:HttpGet('https://pastefy.app/tZktsrcy/raw'))()
+        loadstring(game:HttpGet('https://pastefy.app/OnFEFN9t/raw'))()
     end
 })
 
